2e01be9c95126e84520e81996513675544d28118
jfranklin1607 <jfranklin1607@gmail.com>
2018-03-16 19:39:24 -0400

    files

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('T-Rex', 'Dinosaurs', 20.75, 7);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Tennis Racket', 'Sporting Goods', 200.57, 29);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('A Brand New Car', 'Auto', 1200.01, 2);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Cat', 'Pets', 76.52, 13);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Couch', 'Furniture', 500.79, 17);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Dishwasher', 'Appliances', 326.75, 24);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Bag of Chips', 'Groceries', 0.50, 2);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('The Great Gatsby', 'Books', 13.57, 7);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Fedora', 'Haberdashery', 175.88, 12);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Invisaline', 'Orthodontic', 999.999, 42);
+INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Mystery Box', 'Mysteries', 100.00, 1);
