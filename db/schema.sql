4caeebc396868d0099db4b47d55240db5cd80de1
msptc <31194857+msptc@users.noreply.github.com>
2018-03-22 14:28:38 -0400

    Add files via upload
    styling for all the pages

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS Warehouse_db;
+CREATE DATABASE Warehouse_db;
+USE Warehouse_db;
+
+CREATE TABLE Products (
+    ItemID INT PRIMARY KEY AUTO_INCREMENT, 
+    productname VARCHAR(255), 
+    departmentname VARCHAR(255), 
+    price DECIMAL(6,2), 
+    StockQuantity INT;
