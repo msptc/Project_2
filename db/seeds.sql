4a32d484f0b708f123b4487ea05529d756f6c679
jfranklin1607 <jfranklin1607@gmail.com>
2018-03-24 10:07:44 -0400

    files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 USE Warehouse_db;
 
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('T-Rex', 'Dinosaurs', 20.75, 7);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Tennis Racket', 'Sporting Goods', 200.57, 29);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('A Brand New Car', 'Auto', 1200.01, 2);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Cat', 'Pets', 76.52, 13);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Couch', 'Furniture', 500.79, 17);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Dishwasher', 'Appliances', 326.75, 24);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Bag of Chips', 'Groceries', 0.50, 2);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('The Great Gatsby', 'Books', 13.57, 7);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Fedora', 'Haberdashery', 175.88, 12);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Invisaline', 'Orthodontic', 999.99, 42);
-INSERT INTO Products(productname, departmentname, price, StockQuantity) VALUES('Mystery Box', 'Mysteries', 100.01, 1);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('T-Rex', 'Dinosaurs', 20.75, 7);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Tennis Racket', 'Sporting Goods', 200.57, 29);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('A Brand New Car', 'Auto', 1200.01, 2);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Cat', 'Pets', 76.52, 13);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Couch', 'Furniture', 500.79, 17);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Dishwasher', 'Appliances', 326.75, 24);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Bag of Chips', 'Groceries', 0.50, 2);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('The Great Gatsby', 'Books', 13.57, 7);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Fedora', 'Haberdashery', 175.88, 12);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Invisaline', 'Orthodontic', 999.99, 42);
+INSERT INTO Products(ItemId, ProductName, Quantity, Price, ProductDetails) VALUES('Mystery Box', 'Mysteries', 100.01, 1);
